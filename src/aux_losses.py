a84092c1b4278b21dea5b238522301a2836a6b53
andres <andresnowak52@gmail.com>
2025-09-11 15:10:35 +0200

    Changed the way we compute the f_i value for load balance

--- src/aux_losses.py
+++ src/aux_losses.py
@@ -1,5 +1,7 @@
 import mlx.core as mx
 
+from .mlx_extension import one_hot
+
 
 def compute_expert_load_balance_loss(
     shared_experts: int,
@@ -10,7 +12,7 @@ def compute_expert_load_balance_loss(
     num_tokens: int,
 ) -> mx.array:
     """
-    Compute Expert load balancing loss to encourage uniform expert usage.
+    Compute Expert load balancing loss to encourage uniform expert usage. As in DeepSeekMoE: https://arxiv.org/abs/2401.06066
 
     Parameters
     ----------
@@ -26,22 +28,62 @@ def compute_expert_load_balance_loss(
     array
         Load balancing loss scalar
     """
-    # Implemented as in DeepSeekMoE: https://arxiv.org/abs/2401.06066
 
     routed_gate_probs = gate_probs[..., shared_experts:]
 
     mean_gate_probs = routed_gate_probs.mean(axis=(0, 1))  # P_i in the paper
 
-    experts_counts = mx.zeros(routed_experts)
+    experts_mask = one_hot(
+        expert_indices, shared_experts + routed_experts
+    )  # (batch, seq_len, selected_experts, total_experts)
 
-    for e in range(shared_experts, shared_experts + routed_experts):
-        e_idx = e - shared_experts
-        mask = (expert_indices == e).any(axis=-1)  # (batch, seq_len)
-        experts_counts[e_idx] = mask.sum()
+    experts_counts = experts_mask.sum(axis=(0, 1, 2))[
+        shared_experts:
+    ]  # shared experts are always the first ones. (num_experts)
 
     experts_counts = experts_counts * (
         routed_experts / (top_k_routers * num_tokens)
     )  # f_i in the paper
+
+    # Load balancing loss: encourage P_i * f_i to be uniform
+    load_balance_loss = (experts_counts * mean_gate_probs).sum()
+
+    return load_balance_loss
+
+
+def compute_load_balance_loss(
+    num_experts: int,
+    gate_probs: mx.array,
+    expert_indices: mx.array,
+    num_tokens: int,
+) -> mx.array:
+    """
+    Compute load balancing loss to encourage uniform expert usage. As in switch transformer https://arxiv.org/abs/2101.03961
+
+    Parameters
+    ----------
+    gate_probs : array
+        Raw gate probabilities (batch, seq_len, total_experts)
+    expert_indices : array
+        Selected expert indices (batch, seq_len, top_k_routers)
+    num_tokens : int
+        Total number of tokens in the batch
+
+    Returns
+    -------
+    array
+        Load balancing loss scalar
+    """
+
+    mean_gate_probs = gate_probs.mean(axis=(0, 1))  # P_i in the paper
+
+    experts_mask = one_hot(
+        expert_indices, num_experts
+    )  # (batch, seq_len, selected_experts, num_experts)
+
+    experts_counts = experts_mask.sum(axis=(0, 1, 2)) # (num_experts)
+
+    experts_counts = experts_counts / num_tokens  # f_i in the paper
 
     # Load balancing loss: encourage P_i * f_i to be uniform
     load_balance_loss = (experts_counts * mean_gate_probs).sum()
--- src/mlx_extension.py
+++ src/mlx_extension.py
@@ -1,10 +1,15 @@
 import mlx.core as mx
 
+
 def multinomial(
     x: mx.array, num_samples: int = 1, replacement: bool = False
 ) -> mx.array:
-    assert 1 <= x.ndim <= 2 and num_samples > 0, f"{x.ndim=} must be 1 or 2 dim, {num_samples=} must be positive"
-    assert replacement or num_samples == 1, "no replacement only supports num_samples = 1"
+    assert 1 <= x.ndim <= 2 and num_samples > 0, (
+        f"{x.ndim=} must be 1 or 2 dim, {num_samples=} must be positive"
+    )
+    assert replacement or num_samples == 1, (
+        "no replacement only supports num_samples = 1"
+    )
 
     weights = mx.expand_dims(x, 0) if x.ndim == 1 else x
 
@@ -16,14 +21,27 @@ def multinomial(
 
     unif_samples = mx.random.uniform(shape=[B, num_samples])
 
-    U_exp   = mx.expand_dims(unif_samples, 2) # (B, num_samples, 1)
-    cdf_exp = mx.expand_dims(cdf, 1) # (B, 1, D)
-    mask    = U_exp <= cdf_exp # (B, num_samples, D)
+    U_exp = mx.expand_dims(unif_samples, 2)  # (B, num_samples, 1)
+    cdf_exp = mx.expand_dims(cdf, 1)  # (B, 1, D)
+    mask = U_exp <= cdf_exp  # (B, num_samples, D)
 
-    indices = mask.argmax(axis=2) # (B, num_samples), so basically here we are grabbing the first value that gives true, because argmax iterates from first position to last and it grabs the first value it finds
+    indices = mask.argmax(
+        axis=2
+    )  # (B, num_samples), so basically here we are grabbing the first value that gives true, because argmax iterates from first position to last and it grabs the first value it finds
 
     # 6) if the original x was 1-D, drop the batch dimension
     if x.ndim == 1:
         indices = indices.reshape((num_samples,))
 
-    return indices # (B, num_samples)
+    return indices  # (B, num_samples)
+
+
+def one_hot(x: mx.array, num_classes: int):
+    # assert x.dtype in [mx.uint8, mx.uint16, mx.uint32, mx.uint64, mx.int8, mx.int16, mx.int32, mx.int64, mx.unsignedinteger, mx.integer]
+
+    orig_shape = x.shape
+
+    eye_mat = mx.eye(num_classes)
+    one_hot = eye_mat[x.reshape(-1)]  # (orig_shape.flatten(), num_classes)
+
+    return one_hot.reshape(*orig_shape, num_classes)
